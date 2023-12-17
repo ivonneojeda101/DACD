6a875b8683ba5984c2705032e652176aa90c5818
Yulieth Ojeda <yulieth9109@gmail.com>
2023-12-17 18:28:23 +0000

    Limpiar Repositorio

--- practice1/src/main/java/org/example/controllers/WeatherStore.java
+++
@@ -1,9 +0,0 @@
-package org.example.controllers;
-
-import org.example.model.Location;
-import org.example.model.Weather;
-import java.util.List;
-public interface WeatherStore extends AutoCloseable {
-	void setUpStore(List<Location> locations);
-	void setWeather(Weather weather);
-}
